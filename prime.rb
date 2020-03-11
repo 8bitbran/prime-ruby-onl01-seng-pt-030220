ed75f685bbbf81eb6ddae3cea00b4c066aafb681
Brandon Marrero <brandonmar97@gmail.com>
2020-03-11 20:07:50 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,12 +1,6 @@
-# Add  code here!
 def prime?(int)
-  if int <= 1
-    false
-  else 
-    if (2...int).any? {|n| int % n == 0}
-      false
-    else
-      true
-    end
-  end
+  if int <= 1 
+    false 
+  else
+    
 end 
